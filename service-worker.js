a9a0f1f7dcebb27022dca6948b1776903b3cb201
JDPlumbing <jdplumbingsoflo@gmail.com>
2025-07-08 18:56:02 -0400

    Enable PWA support with icons and offline caching

---
+++ service-worker.js
@@ -0,0 +1,37 @@
+// service-worker.js
+const CACHE_NAME = 'chaincode-wallet-cache-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/main.js',
+  '/style.css',
+  '/manifest.json',
+  '/icon-192.png',
+  '/icon-512.png'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then(resp => {
+      return resp || fetch(event.request);
+    })
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then(keys => {
+      return Promise.all(
+        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+      );
+    })
+  );
+});
